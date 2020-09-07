42f3b4f1a3c59c9756004680700671b5a815e355
Sammy-Barasa <kesammybarasa@gmail.com>
2020-09-07 20:37:33 +0300

    production build

---
+++ build/precache-manifest.aa8066530f205c3b4f191ec72005cbdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5943e486206a7a2d4bdd214d408bfa08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0606ec3e3901db461d4c",
+    "url": "/static/js/2.04da9399.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04da9399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f962d18a52790353d2",
+    "url": "/static/js/main.e03711a0.chunk.js"
+  },
+  {
+    "revision": "a626a241f4318790208b",
+    "url": "/static/js/runtime-main.d3a9e4dc.js"
+  }
+]);
